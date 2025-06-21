44c98be79bdf28dd4900b3cdce7520bbc4468f84
greydev <greyc9404@gmail.com>
2025-06-20 22:40:34 -0400

    creacion de modelos polls

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
